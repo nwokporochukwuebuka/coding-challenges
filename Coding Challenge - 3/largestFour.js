7491641fa9c5f43e7e671528f435517df7bb822b
Chukwuebuka2 <nwokporochukwuebuka@gmail.com>
2022-08-16 11:12:15 +0100

    done

--- Coding Challenge - 3/largestFour.js
+++ Coding Challenge - 3/largestFour.js
@@ -1,3 +1,13 @@
+/*
+Question: Largest Four
+
+Have the function LargestFour(arr) take the array of integers stored in arr, 
+and find the four largest elements and return their sum. For example: if arr is [4, 5, -2, 3, 1, 2, 6]
+then the four largest elements in the array are 6, 6, 4 and 5 and the total sum of numbers 21, so your program should
+return 21. If there are less than four numbers in the array your program should return the sum of all the numbers in
+the array
+*/
+
 function LargestFour(arr){
     let maxNumbers = [];
 
