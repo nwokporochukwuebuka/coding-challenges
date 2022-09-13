d0bd90612e2c55c1cd0e3bf5c8882880f225758f
Chukwuebuka2 <nwokporochukwuebuka@gmail.com>
2022-09-13 22:25:16 +0100

    group Totals updated

--- Coding Challenge - 6/groupTotals.js
+++ Coding Challenge - 6/groupTotals.js
@@ -16,4 +16,39 @@ function groupTotals(strArr){
     return JSON.stringify(answers).replace(/["]/g, '').replace(/[{}]/g, '')
 }
 
-console.log(groupTotals(testString));
+// console.log(groupTotals(testString));
+
+
+// Tosin's Solution
+function GroupTotal(strApp){
+    hash = {};
+
+    for(let i = 0; i < strApp.length; i++){
+        newStr = strApp[i].split(":");
+        key = newStr[0];
+        value = parseInt(newStr[1]);
+
+        if (hash.hasOwnProperty(key)){
+            hash[key] += value;
+        } else {
+            hash[key] = value;
+        }
+    }
+
+    hash = Object.entries(hash);
+
+    hash = hash.sort();
+
+    const newHash = new Map([...hash].filter(([k, v]) => v > 0));
+    ans = "";
+    for(let [key, value] of newHash){
+        ans += key;
+        ans += ":";
+        ans += value;
+        ans += ",";
+    }
+    ans = ans.slice(0, -1);
+    return ans;
+}
+
+console.log(GroupTotal(testString));
