efeb079437f9570829fa78e054e015f9a6bf3be6
Chukwuebuka2 <nwokporochukwuebuka@gmail.com>
2022-09-14 11:59:55 +0100

    commenting

--- Coding Challenge - 6/groupTotals.js
+++ Coding Challenge - 6/groupTotals.js
@@ -2,22 +2,31 @@
 let testString = ["X:-1", "Y:1", "X:7", "B:2", "X:5", "Y:8", "A:5"]
 
 function groupTotals(strArr){
+
+    // creating an object to store my result
     let answers = {};
 
+    // looping through the array and making sure that the keys and their values are properly stored in the object
     for(let i = 0; i < strArr.length; i++){
+
+        // if the key does not exist in the object then create a new instance
         if(strArr[i][0] in answers === false){
-            answers[strArr[i][0]] = Number(strArr[i].slice(2, 4));
+            answers[strArr[i][0]] = Number(strArr[i].slice(2, 5));
+
+            // else if it exist then add up
         }else {
-            answers[strArr[i][0]] += Number(strArr[i].slice(2, 4));
+            answers[strArr[i][0]] += Number(strArr[i].slice(2, 5));
         }
     }
 
+    // sorting the object
     answers = Object.keys(answers).sort().reduce((accumulator, key) => {
         accumulator[key] = answers[key];
     
         return accumulator;
       }, {});;
 
+      // turning the object into a string and replacing the curly braces and quotation
     return JSON.stringify(answers).replace(/["]/g, '').replace(/[{}]/g, '')
 }
 
@@ -28,13 +37,23 @@ console.log(groupTotals(testString));
 function GroupTotal(strApp){
     hash = {};
 
+    // looping through the array and performing the following operations
     for(let i = 0; i < strApp.length; i++){
+
+        // splitting based on the colon
         newStr = strApp[i].split(":");
+
+        // taking the key
         key = newStr[0];
+
+        // taking the value
         value = parseInt(newStr[1]);
 
+        // if the key already exist in the object, then add up
         if (hash.hasOwnProperty(key)){
             hash[key] += value;
+
+            // creating a new value
         } else {
             hash[key] = value;
         }
